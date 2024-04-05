70b96d23a8438e167ca80f23ee1af76480326f06
JeetLi <jeetwork168@gmail.com>
2024-04-05 13:59:42 +0300

    feat: add component partners

---
+++ src/components/partners/partners.tsx
@@ -0,0 +1,87 @@
+import React from "react";
+
+export const Partners: React.FC = () => {
+  return (
+    <div className="partners partners_contacts">
+      <div className="container">
+        <p>
+          «Будем Строить.ru» – динамично развивающаяся и успешно
+          зарекомендовавшая себя компания, осуществляющая продажу
+          стройматериалов оптом не только в Москве, но и в регионах России.
+          Данная информация взята с сайта компании «Будем Строить»
+        </p>
+        <div className="partners_wrap">
+          <h2 className="title">
+            наши <b>партнеры</b>
+          </h2>
+          <div className="partners_list row">
+            <div className="partner_item">
+              <img
+                decoding="async"
+                loading="lazy"
+                src="/images/logos/image15.png"
+                alt="Partner Logo"
+              />
+            </div>
+            <div className="partner_item">
+              <img
+                decoding="async"
+                loading="lazy"
+                src="/images/logos/image11.png"
+                alt="Partner Logo"
+              />
+            </div>
+            <div className="partner_item">
+              <img
+                decoding="async"
+                loading="lazy"
+                src="/images/logos/image12.png"
+                alt="Partner Logo"
+              />
+            </div>
+            <div className="partner_item">
+              <img
+                decoding="async"
+                loading="lazy"
+                src="/images/logos/image18.png"
+                alt="Partner Logo"
+              />
+            </div>
+            <div className="partner_item">
+              <img
+                decoding="async"
+                loading="lazy"
+                src="/images/logos/image9.png"
+                alt="Partner Logo"
+              />
+            </div>
+            <div className="partner_item">
+              <img
+                decoding="async"
+                loading="lazy"
+                src="/images/logos/image16.png"
+                alt="Partner Logo"
+              />
+            </div>
+            <div className="partner_item">
+              <img
+                decoding="async"
+                loading="lazy"
+                src="/images/logos/image19.png"
+                alt="Partner Logo"
+              />
+            </div>
+            <div className="partner_item">
+              <img
+                decoding="async"
+                loading="lazy"
+                src="/images/logos/image20.png"
+                alt="Partner Logo"
+              />
+            </div>
+          </div>
+        </div>
+      </div>
+    </div>
+  );
+};
